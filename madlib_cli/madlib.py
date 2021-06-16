0bf44f7a15201666c81543b054dbf8f88c26bf45
WondwosenTsige <wondneh@gmail.com>
2021-06-16 11:52:17 -0700

    3 tests passed and one failed

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -4,23 +4,30 @@ def greetings():
     print( """
 
     **************************************
-    **    Welcome to the madlib game!   **
+        Welcome to the madlib game!
     
     
-    ** Instructions to play this game   **
+     Instructions to play this game   
     **************************************
     """)
 
 def read_template(filePath):
+    #Open and read a file
     with open(filePath, "r") as target_file:
         file_contents = target_file.read()
         return file_contents
 
 
-def parse_template(words):
-    
+def parse_template(string):
+    #Find the string part and return tuple
+    #Replace the string and return string
+    stripped_words = tuple(re.findall(r"({\w*})", string))
+    string = re.sub(r"\{.*?\}", "{}", string)
+    print(string, stripped_words)
+    return (string, stripped_words)
 
 
-def merge(words, tuple):
+def merge(string, new):
+    return string.format(*new)
 
 
